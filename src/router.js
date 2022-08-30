6ef084f20983eaa9c02c7a8ffd17984cb898c848
seungmin lim <seungmin3238@gmail.com>
2022-08-30 13:19:29 +0900

    초기세팅 경로추가

--- src/router.js
+++ src/router.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Host from './pages/Host/Host';
+import Detail from './pages/Detail/Detail';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/host" element={<Host />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
