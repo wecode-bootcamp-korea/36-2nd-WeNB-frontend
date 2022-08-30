e8fa5a3526bbb84cf12701aac1ba7b605559271a
seungmin lim <seungmin3238@gmail.com>
2022-08-30 13:26:34 +0900

    초기세팅 에러수정

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import variables from './styles/variables';
-import Router from './Router';
+import Router from './router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={{ style: theme, variables }}>
