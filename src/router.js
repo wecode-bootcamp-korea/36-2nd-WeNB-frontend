b8de1b59f480c718c43525bbb063b20cf35a8d3b
seungmin lim <seungmin3238@gmail.com>
2022-08-30 14:10:00 +0900

    초기세팅 라우터 수정

--- src/router.js
+++ src/router.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 import Main from './pages/Main/Main';
-import Host from './pages/Host/Host';
 import Detail from './pages/Detail/Detail';
+import Host from './pages/Host/Host';
+
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/host" element={<Host />} />
-        <Route path="/detail" element={<Detail />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/Detail" element={<Detail />} />
+          <Route path="/Host" element={<Host />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 export default Router;
