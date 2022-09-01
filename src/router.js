a1ac83bc97a82b3026251d08fca1072418745d95
이빛나 <bitnalee@ibichnaui-MacBookAir.local>
2022-09-01 18:41:13 +0900

    Footer구현완료

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
 import Host from './pages/Host/Host';
 import KakaoLoding from './components/Login/components/KakaoLoding';
+import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
@@ -16,6 +17,7 @@ const Router = () => {
           <Route path="/Detail" element={<Detail />} />
           <Route path="/Host" element={<Host />} />
           <Route path="/kakao" element={<KakaoLoding />} />
+          <Route path="/Footer" element={<Footer />} />\{' '}
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
