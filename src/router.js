85a4307f5c5ed42163db795d06f3d7c485a59beb
seungmin lim <seungmin3238@gmail.com>
2022-09-02 10:03:04 +0900

    소셜 로그인 기능 구현 완료

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import theme from './styles/theme';
 import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
 import Host from './pages/Host/Host';
+import KakaoLoding from './components/Login/components/KakaoLoding';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
           <Route path="/" element={<Main />} />
           <Route path="/Detail" element={<Detail />} />
           <Route path="/Host" element={<Host />} />
+          <Route path="/kakao" element={<KakaoLoding />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
